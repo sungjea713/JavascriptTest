38de7e7b1a5167b4d3b5630be92f338fc2979327
Sungjea Kim <sjkim@smartradarsystem.com>
2019-09-27 15:40:22 +0900

    Chrome browser test cannot be supported

--- WebTest/static/js/threadTest.js
+++ WebTest/static/js/threadTest.js
@@ -1,6 +1,7 @@
 var w;
 function workerStart(){
     if(typeof(w) == 'undefined'){
+        //Chrome browser cannot support public Worker
         w = new Worker('worker.js');
     }
     w.onmessage = function(response) {
